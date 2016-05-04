a14b73a1893518864d8389611de502d0cba2c3b6
Aggrey Jacobs <aggreyj@gmail.com>
2016-05-03 18:02:21 -0700

    Configure account on sign out.

--- GoogleSignIn/DriveViewController.swift
+++ GoogleSignIn/DriveViewController.swift
@@ -5,6 +5,8 @@ class DriveViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDeleg
   @IBOutlet weak var tableVIew: UITableView!
   @IBOutlet weak var accountView: GoogleAccountView!
 
+  var driveDataSource: GoogleDriveDataSource?
+
   override func viewDidLoad() {
     super.viewDidLoad()
     GIDSignIn.sharedInstance().delegate = self
@@ -20,15 +22,20 @@ class DriveViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDeleg
 
   }
 
+  func refreshTable() {
+    self.tableVIew.reloadData()
+  }
+
   func signIn(signIn: GIDSignIn!, didSignInForUser user: GIDGoogleUser!, withError error: NSError!) {
     if error == nil {
       self.accountView.configureAccountView(true, profile: user.profile)
+      self.driveDataSource = GoogleDriveDataSource(user: user, parent: self)
     }
 
   }
 
   func signIn(signIn: GIDSignIn!, didDisconnectWithUser user: GIDGoogleUser!, withError error: NSError!) {
-
+    self.accountView.configureAccountView(false, profile: user.profile)
   }
 
 }
